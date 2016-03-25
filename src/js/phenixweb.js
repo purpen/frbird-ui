30458a70d393fdb4b71e1d84c88a9f2478715e56
lyf <987083180@qq.com>
2016-03-25 21:40:02 +0800

    PC mobile

---
+++ src/js/phenixweb.js
@@ -0,0 +1,10 @@
+//web 下载二维码
+$(window).scroll(function(){
+	var scrollTop = $(this).scrollTop();
+	if ( scrollTop < 200 || scrollTop > $('#ft').offset().top-650 ){
+		$('.iosewm img').fadeOut(800);
+	}
+	if( scrollTop > 200 && scrollTop < $('#ft').offset().top-650 ){
+		$('.iosewm img').fadeIn(800);
+	}
+});
